f768a8105d39eae0a2f0a162df16388a36bc8fda
jasong <jasong@student.42seoul.kr>
2025-01-18 15:12:12 +0900

    fix: add cors middleware on fastapi

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from enum import Enum
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, SessionLocal, engine
 from models import ForecastUser
@@ -34,6 +35,16 @@ def generate_hash_id(data: dict) -> str:
     return hash_object.hexdigest()[:8]  # Take first 8 characters of the hash
 
 app = FastAPI()
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Gender(str, Enum):
     MALE = "Male"
